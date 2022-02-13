4a9754a3f812d02ff3496f6786dce47691635ec8
EvgeniyTen <repeatitplease@gmail.com>
2022-02-13 14:17:40 +0300

    Initial Commit

---
+++ TestTableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTableViewApp
+//
+//  Created by Евгений Тимофеев on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
